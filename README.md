80ebbac86d78252fd214899ffdab310b30c06f7e
Benito <sanchez.benitog95@hotmail.com>
2015-12-09 05:44:46 +0000

    added the .sql file that creates the schema

---
+++ README.md
@@ -0,0 +1,7 @@
+# SQLProject 
+
+Database Design Project
+
+ER Diagram
+
+We have developed an ER Diagram for a drug store chain.
---
+++ project.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS patient;
+CREATE TABLE patient(
+	  ssn				INT(8),
+	  patient_name				VARCHAR(20),
+	  age				INT(3),
+	  building_number			INT(4),
+	  street			VARCHAR(30),
+	  city				VARCHAR(20),
+	  state				VARCHAR(20),
+	  zipcode			INT(5),
+	  primary Key(ssn)
+);
+
+DROP TABLE IF EXISTS doctor;
+CREATE TABLE doctor(
+	   ssn	 INT(8),
+	   doctor_name		VARCHAR(20),
+	   specialty		VARCHAR(30),
+	   years_of_experience		INT(3),
+);
+
+DROP TABLE IF EXISTS drug;
+CREATE TABLE drug(
+	   trade_name		VARCHAR(30),
+	   formula			VARCHAR(30)
+	   primary key(trade_name)
+);
+
+DROP TABLE IF EXISTS pharmacy;
+CREATE TABLE pharmacy(
+	   pharmacy_name	VARCHAR(30),
+	   building_number			INT(4),
+	   street			VARCHAR(30),
+	   city				VARCHAR(20),
+	   state			VARCHAR(20),
+	   zipcode			INT(5),
+	   primary key(pharmacy_name)
+);
+
+DROP TABLE IF EXISTS pharmaceutical_company;
+CREATE TABLE pharmaceutical_company(
+	   company_name		VARCHAR(30),
+	   building_number			INT(4),
+	   street			VARCHAR(30),
+	   city				VARCHAR(20),
+	   state			VARCHAR(20),
+	   zipcode			INT(5),
+	   phone_number		INT(10),
+	   primary key(company_name)
+);
